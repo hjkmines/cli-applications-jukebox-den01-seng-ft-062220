ffbb4116fbd160a7e500019e1ebbf5a800cd30e8
Hyung Kim <hjkmines@gmail.com>
2020-06-19 23:32:18 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,7 +22,7 @@ def play(songs)
   
  puts "Please enter a song name or number:"
   input = gets.strip 
-  output = ""
+  #output = ""
   songs.each_with_index do |song, index|
     if input == (index + 1).to_s || input = song
       puts "Playing #{song}"
