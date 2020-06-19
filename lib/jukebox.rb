eda54ca87a8a2f38d39c0146f7c6b9ef9ba08861
Hyung Kim <hjkmines@gmail.com>
2020-06-19 23:36:16 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,10 +26,9 @@ def play(songs)
     if input == (index + 1).to_s || input == song
       puts "Playing #{song}"
       return song 
-    else 
     end
   end 
-  
+    puts "Invalid input, please try again"
 end 
 
 def exit_jukebox 
